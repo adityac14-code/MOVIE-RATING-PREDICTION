80a0019ddb4220d611ebfc1f7a7a6d33d33c6aba
adityac14-code <121824546+adityac14-code@users.noreply.github.com>
2023-11-28 23:56:47 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='=movie-rating-predictor',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'scikit-learn'
+        
+    ],
+    author='Chitte Aditya',
+    author_email='chitteaditya1@gmail.com',
+    description='Movie Rating Prediction',
+    url='https://github.com/adityac14-code/MOVIE-RATING-PREDICTION',
+    
+)
